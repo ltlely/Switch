00ded13a3189d9fca4129be39696f5942fdfc839
ltlely <96932263+ltlely@users.noreply.github.com>
2022-08-27 21:24:40 -0400

    Add files via upload

---
+++ Main(8).java
@@ -0,0 +1,34 @@
+/*  Write a switch statement that checks origLetter. If 'a' or 'A', print "Alpha". If 'b' or 'B', print 
+"Beta". For any other character, print "Unknown". Use fall-through as appropriate. End with newline. */
+
+
+import java.util.Scanner;
+
+public class ConvertToGreek {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      char origLetter;
+
+      origLetter = scnr.next().charAt(0);
+
+      /* Your solution goes here  */
+      
+      switch (origLetter) {
+      
+      case 'a': 
+      case 'A':
+         System.out.println ("Alpha");
+         break;
+      
+      case 'b':
+      case 'B':
+         System.out.println ("Beta");
+         break;
+      
+      default: 
+         System.out.println ("Unknown");
+     
+      }
+
+   }
+}
